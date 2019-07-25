2baca32c3ff8cbfa50d18d026d963dfdbe78991b
MrT3313 <rturgeon13@gmail.com>
2019-07-25 14:17:13 -0500

    FE <Login />
    Component level state updating on user form input

--- frontEnd/home_manager/src/views/Login.js
+++ frontEnd/home_manager/src/views/Login.js
@@ -6,10 +6,16 @@ import { Link } from 'react-router-dom'
 // -- *** -- START CODE -- *** -- //
 
 class LoginPage extends Component {
+    state = {
+        userName: undefined,
+        pw: undefined,
+    }
+    
     render() {
         return (
             <div className="Container">
                 <h1>Login Page</h1>
+
                 <div className="DummyNav">
                     <h1>Landing Page</h1>
                     <Link to="/">Landing Page</Link>
@@ -21,8 +27,37 @@ class LoginPage extends Component {
                     <Link to="/register">Register Page</Link>
                     <br />
                 </div>
+
+                <div className='registerForm'>
+                    <form onSubmit={this.registerSubmit}>
+                        <input
+                            id='userName'
+                            type='text'
+                            placeholder='First Name'
+                            onChange={this.inputChange}
+                        ></input>
+                        <input
+                            id='pw'
+                            type='text'
+                            placeholder='Last Name'
+                            onChange={this.inputChange}
+                        ></input>
+                        <button>
+                            Submit
+                        </button>
+                    </form>
+                </div>
             </div>
         );
+    }
+
+    // -- Methods -- //
+    inputChange = e => {
+        this.setState({ [e.target.id]: e.target.value })
+    }
+    
+    registerSubmit = e => {
+
     }
 }
 
